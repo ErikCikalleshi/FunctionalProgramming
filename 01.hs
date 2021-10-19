02d99da32541de1249932a087188ce607fc8861b
ErikCikalleshi <erik.ck@outlook.it>
2021-10-19 11:50:49 +0200

    Added 01 + 02 Projects

---
+++ 01.hs
@@ -0,0 +1,23 @@
+
+milesToKilometers m = m * 1.609347
+
+volume r = (4/3) * pi * r ** 3
+
+average x y = (x + y)/2
+
+{- Exercise 2 ->   Nr 4. 
+    When executing "average (average x y) z" it actually calculates first the average in Brackets between two numbers and the results is beeing used
+    as x for the average with z 
+-}
+
+averageVolume r1 r2 = average (volume r1)(volume r2) 
+  
+
+-- the following tests can be used by you, once you have implemented your functions,
+-- for testing, just invoke test1, test2, ... in ghci
+
+test1 = "test1: expected ~ 107.826048, computed: " ++ show (milesToKilometers 67 :: Double)
+test2 = "test2: expected ~ 179.59438, computed: " ++ show (volume 3.5 :: Double)
+test3 = "test3: expected ~ 2.070796, computed: " ++ show (average pi 1 :: Double)
+test4 = "test4: expected ~ 150.796448, computed: " ++ show (averageVolume 2 4 :: Double)
+
---
+++ 02.hs
@@ -0,0 +1,48 @@
+plus1 x = x + 1
+ 
+{-
+   "0 :: Bool" --> not posible because type Bool expects only the type 'true' or 'false
+  
+   'head "test" :: Char' --> would work, but it doesn't make any sense for the 'plus1' function
+ 
+   'hello':: String --> single quotation marks are used only for chars; to make this work we would use double quotation marks
+ 
+   "plus1 :: Integer -> Integer" --> it defines that the function called "plus1" is taking as argument a Integer
+       and it returns an Integer
+ 
+-}
+
+
+{-data Menu = -- name of type
+  Menu       -- name of constructor
+    Item
+    Item
+    Item
+deriving Show
+
+data Item = -- name of type
+  Item      
+    String    -- label name
+    String   --link
+deriving Show
+
+itemFP = Item "Home" "http://cl-informatik.uibk.ac.at/teaching/ws21/fp"
+itemOO = Item "Home" "http://cl-informatik.uibk.ac.at/teaching/ws21/fp"
+checkMenu = Menu itemFP itemOO -}
+
+data Item = -- name of type
+  Item      
+    String    -- label name
+    String   --link
+  deriving Show 
+
+data Menu = -- name of type
+    Empty 
+  | Menu Item Menu      -- name of constructor
+  deriving Show
+
+
+
+itemFP = Item "Home" "http://cl-informatik.uibk.ac.at/teaching/ws21/fp"
+itemOO = Item "Home" "https://lms.uibk.ac.at"
+checkMenu = Menu itemOO(Menu itemFP Empty)
