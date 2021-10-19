7a210efd60dfee0e68a847121e607fcd2cbc8a18
ErikCikalleshi <erik.ck@outlook.it>
2021-10-19 12:44:35 +0200

    Fixed 02

--- 02.hs
+++ 02.hs
@@ -28,6 +28,24 @@ deriving Show
 itemFP = Item "Home" "http://cl-informatik.uibk.ac.at/teaching/ws21/fp"
 itemOO = Item "Home" "http://cl-informatik.uibk.ac.at/teaching/ws21/fp"
 checkMenu = Menu itemFP itemOO -}
+{-
+data Item = -- name of type
+  Item      
+    String    -- label name
+    String    --link
+  deriving Show 
+
+data Menu = -- name of type
+    Empty 
+  | Menu Item Menu      -- name of constructor
+  deriving Show
+
+
+
+itemFP = Item "Home" "http://cl-informatik.uibk.ac.at/teaching/ws21/fp"
+itemFP2 = Item "Proseminar" "http://cl-informatik.uibk.ac.at/teaching/ws21/fp/exercises.php?lan=de"
+itemOO = Item "Home" "https://lms.uibk.ac.at"
+checkMenu = Menu itemOO(Menu itemFP(Menu itemFP2 Empty))-}
 
 data Item = -- name of type
   Item      
